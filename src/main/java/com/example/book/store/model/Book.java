a7fd240a773f75d80f6b99303ff08cd5b4c70db2
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2023-09-10 17:31:49 +0300

    Added Book entity, repository and service

--- src/main/java/com/example/book/store/model/Book.java
+++ src/main/java/com/example/book/store/model/Book.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Entity
@@ -15,14 +14,13 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotNull
+    @Column(nullable = false)
     private String title;
-    @NotNull
+    @Column(nullable = false)
     private String author;
-    @NotNull
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String isbn;
-    @NotNull
+    @Column(nullable = false)
     private BigDecimal price;
     private String description;
     private String coverImage;
