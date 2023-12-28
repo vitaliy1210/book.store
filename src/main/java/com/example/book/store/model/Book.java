67408931a34df06664e029855ee5124a94064d9f
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2023-12-28 04:43:24 +0200

    Implemented soft delete concept for Book entity, added liquibase support and possibility to update book by id

--- src/main/java/com/example/book/store/model/Book.java
+++ src/main/java/com/example/book/store/model/Book.java
@@ -9,11 +9,15 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
-@Entity
-@Table(name = "books")
 @Getter
 @Setter
+@Entity
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted = false")
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +32,8 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 
     @Override
     public String toString() {
